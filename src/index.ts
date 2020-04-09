b6f5b8c823f7aef33ba2cf7bb02e90750c7b6d9d
Fralez <franco.gonzalez.burgos@gmail.com>
2020-04-09 00:24:12 -0300

    Song model created; extract song info method done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 "use strict";
 
-import Telegraf from "telegraf";
 import { config } from "dotenv-safe";
+import Telegraf from "telegraf";
+
+import { generateSong, Song } from "./models/Song";
 
 // Config .env variables into process.env
-config({
-  allowEmptyValues: true,
-});
+config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN || "");
+
+bot.on("message", async (ctx) => {
+  // "https://youtu.be/jAt9VZpMIyM"
+  const song: Song = await generateSong("https://youtu.be/jAt9VZpMIyM");
+  ctx.reply(song.url);
+});
+
+bot.startPolling();
---
+++ src/models/Song.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosResponse } from "axios";
+
+interface ISong {
+  id: string;
+  url: string;
+  title: string;
+  artist?: string;
+  album?: string;
+  cover?: string;
+  duration: number;
+  bytesSize: number;
+}
+
+export class Song implements ISong {
+  public id: string;
+  public url: string;
+  public title: string;
+  public artist?: string;
+  public album?: string;
+  public cover?: string;
+  public duration: number;
+  public bytesSize: number;
+
+  constructor(song: ISong) {
+    this.id = song.id;
+    this.url = song.url;
+    this.title = song.title;
+    this.artist = song.artist;
+    this.album = song.album;
+    this.cover = song.cover;
+    this.duration = song.duration;
+    this.bytesSize = song.bytesSize;
+  }
+
+  public async download(): Promise<boolean> {
+    return false;
+  }
+}
+
+export async function generateSong(ytUrl: string): Promise<Song> {
+  const response: AxiosResponse = await axios.get(
+    `${process.env.BASE_URL}/song/info?ytUrl=${ytUrl}`
+  );
+
+  const songInfo = response.data.info;
+
+  const song: ISong = {
+    id: songInfo.id,
+    url: songInfo.url,
+    title: songInfo.title,
+    artist: songInfo.artist,
+    album: songInfo.album,
+    cover: songInfo.cover,
+    duration: songInfo.duration,
+    bytesSize: songInfo.bytes_size,
+  };
+
+  return new Song(song);
+}
