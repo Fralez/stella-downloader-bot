4304543b3f60e2988c185ce06527c128b5e08efc
Fralez <franco.gonzalez.burgos@gmail.com>
2020-04-09 23:56:14 -0300

    Added i18n locale module

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 "use strict";
 
 import { config } from "dotenv-safe";
-import Telegraf from "telegraf";
-
-import { generateSong, Song } from "./models/Song";
+import Telegraf, { Markup } from "telegraf";
+import i18n from "i18n";
 
 // Config .env variables into process.env
 config();
 
+i18n.configure({
+  locales: ["en"],
+  directory: __dirname + "/../locales",
+});
+
 const bot = new Telegraf(process.env.BOT_TOKEN || "");
 
-bot.on("message", async (ctx) => {
-  ctx.reply("Hello human! Meet my handsome creator: @fralezz");
+bot.command(["start", "help"], ({ reply }) => {
+  return reply(i18n.__("purpose"));
 });
 
 bot.startPolling();
